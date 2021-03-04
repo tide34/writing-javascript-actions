a8617ab63baa655ad8c32c9f48f6ae54128f537b
tide34 <gleb.kragel@gmail.com>
2021-03-05 00:28:33 +0300

    create index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -10,7 +10,7 @@ async function run() {
         repo: github.context.repo.repo,
         owner: github.context.repo.owner,
         title: issueTitle,
-        body; jokeBody
+        body: jokeBody
     });
   } catch (err) {
       core.setFailed(err.message);
